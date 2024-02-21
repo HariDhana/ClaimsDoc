d7fbc8d20e1e92c3a008bfa7cfe13a798e58237d
HariDhana <haridhana96@gmail.com>
2024-02-21 17:04:26 +0530

    Changes- 21/02/2024

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Start With Claims</span>,
+  logo: (
+    <img src="../images/logo.png" alt="Start With Claims" style={{ maxHeight: '50px' }} />
+  ),
   // project: {
   //   link: 'https://github.com/shuding/nextra-docs-template',
   // },
@@ -11,7 +13,7 @@ const config: DocsThemeConfig = {
   // },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Claim-Assist',
   },
 }
 
